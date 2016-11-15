c49ba17dddde697dcb44c5b10f364334a5dc0a8b
Dillan Johnson <dillan442@yahoo.com>
2016-11-15 11:29:28 -0800

    alott was done

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,9 +1,66 @@
 // Write all your JavaScript code in this file!
 // You can use the namespace variable that's defined here.
 var namespace = "http://www.w3.org/2000/svg"
+
 function topLeft()
 {
   var canvas = document.getElementById("game-board")
-  var rect = document.getElementById("topleft")
+  var rect = document.getElementById("topLeft")
+  rect.setAttribute("fill","orange")
+}
+
+function topMiddle()
+{
+  var canvas = document.getElementById("game-board")
+  var rect = document.getElementById("topMiddle")
+  rect.setAttribute("fill","orange")
+}
+
+function topRight()
+{
+  var canvas = document.getElementById("game-board")
+  var rect = document.getElementById("topRight")
+  rect.setAttribute("fill","orange")
+}
+
+function middleLeft()
+{
+  var canvas = document.getElementById("game-board")
+  var rect = document.getElementById("middleLeft")
+  rect.setAttribute("fill","orange")
+}
+
+function mid()
+{
+  var canvas = document.getElementById("game-board")
+  var rect = document.getElementById("mid")
+  rect.setAttribute("fill","orange")
+}
+
+function middleRight()
+{
+  var canvas = document.getElementById("game-board")
+  var rect = document.getElementById("middleRight")
+  rect.setAttribute("fill","orange")
+}
+
+function bottomLeft()
+{
+  var canvas = document.getElementById("game-board")
+  var rect = document.getElementById("bottomLeft")
+  rect.setAttribute("fill","orange")
+}
+
+function bottomMiddle()
+{
+  var canvas = document.getElementById("game-board")
+  var rect = document.getElementById("bottomMiddle")
+  rect.setAttribute("fill","orange")
+}
+
+function bottomRight()
+{
+  var canvas = document.getElementById("game-board")
+  var rect = document.getElementById("bottomRight")
   rect.setAttribute("fill","orange")
 }
