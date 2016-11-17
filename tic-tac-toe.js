3d4ec42c6e70717abe6014a861cbcf269acd8bb6
Dillan Johnson <dillan442@yahoo.com>
2016-11-17 11:25:32 -0800

    basically done

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -2,15 +2,17 @@
 // You can use the namespace variable that's defined here.
 var namespace = "http://www.w3.org/2000/svg"
 
+
+var rectColor="white"
 function topLeft()
 {
   var canvas = document.getElementById("game-board")
   var rect = document.getElementById("topLeft")
-  if (rectcolor=="orange") {
-    rect.setAttribute("fill","white")
+  if (rectColor=="orange") {
+    rect.setAttribute("fill", "white")
     rectColor="white"
   } else{
-    rect.setAttribute("fill","orange")
+    rect.setAttribute("fill", "orange")
     rectColor="orange"
   }
 }
@@ -25,54 +27,136 @@ function topMiddle()
 {
   var canvas = document.getElementById("game-board")
   var rect = document.getElementById("topMiddle")
-  rect.setAttribute("fill","orange")
+  if (rectColor=="orange") {
+    rect.setAttribute("fill", "white")
+    rectColor="white"
+  } else{
+    rect.setAttribute("fill", "orange")
+    rectColor="orange"
+  }
 }
+
+
+
+
+
+
 
 function topRight()
 {
   var canvas = document.getElementById("game-board")
   var rect = document.getElementById("topRight")
-  rect.setAttribute("fill","orange")
+  if (rectColor=="orange") {
+    rect.setAttribute("fill", "white")
+    rectColor="white"
+  } else{
+    rect.setAttribute("fill", "orange")
+    rectColor="orange"
+  }
 }
+
+
+
+
+
 
 function middleLeft()
 {
   var canvas = document.getElementById("game-board")
   var rect = document.getElementById("middleLeft")
-  rect.setAttribute("fill","orange")
+  if (rectColor=="orange") {
+    rect.setAttribute("fill", "white")
+    rectColor="white"
+  } else{
+    rect.setAttribute("fill", "orange")
+    rectColor="orange"
+  }
 }
+
+
+
+
+
 
 function mid()
 {
   var canvas = document.getElementById("game-board")
   var rect = document.getElementById("mid")
-  rect.setAttribute("fill","orange")
+  if (rectColor=="orange") {
+    rect.setAttribute("fill", "white")
+    rectColor="white"
+  } else{
+    rect.setAttribute("fill", "orange")
+    rectColor="orange"
+  }
 }
+
+
+
+
+
 
 function middleRight()
 {
   var canvas = document.getElementById("game-board")
   var rect = document.getElementById("middleRight")
-  rect.setAttribute("fill","orange")
+  if (rectColor=="orange") {
+    rect.setAttribute("fill", "white")
+    rectColor="white"
+  } else{
+    rect.setAttribute("fill", "orange")
+    rectColor="orange"
+  }
 }
+
+
+
+
 
 function bottomLeft()
 {
   var canvas = document.getElementById("game-board")
   var rect = document.getElementById("bottomLeft")
-  rect.setAttribute("fill","orange")
+  if (rectColor=="orange") {
+    rect.setAttribute("fill", "white")
+    rectColor="white"
+  } else{
+    rect.setAttribute("fill", "orange")
+    rectColor="orange"
+  }
 }
+
+
+
+
 
 function bottomMiddle()
 {
   var canvas = document.getElementById("game-board")
   var rect = document.getElementById("bottomMiddle")
-  rect.setAttribute("fill","orange")
+  if (rectColor=="orange") {
+    rect.setAttribute("fill", "white")
+    rectColor="white"
+  } else{
+    rect.setAttribute("fill", "orange")
+    rectColor="orange"
+  }
 }
+
+
+
+
+
 
 function bottomRight()
 {
   var canvas = document.getElementById("game-board")
   var rect = document.getElementById("bottomRight")
-  rect.setAttribute("fill","orange")
+  if (rectColor=="orange") {
+    rect.setAttribute("fill", "white")
+    rectColor="white"
+  } else{
+    rect.setAttribute("fill", "orange")
+    rectColor="orange"
+  }
 }
